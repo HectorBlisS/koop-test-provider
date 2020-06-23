bda74b2bc2ff1e44db4ceee005edc124be4816d4
hectorbliss <contacto@fixter.org>
2020-06-23 09:39:54 -0500

    simplest demo ready

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-path = require('path')
+// path = require('path')
 
-module.exports = function(options = {}) {
-    // Allow name to be set by options
-    const name = options.name ? options.name : 'fulcrum-provider' // ENV var instead?
-    const Model = options.model ? options.model : path.join(__dirname, 'model.js')
-    const routes = options.routes ? options.routes : path.join(__dirname, 'routes.js')
-    const Controller = options.controller ? options.controller : path.join(__dirname, 'controllers.js')
+// module.exports = function(options = {}) {
+//     // Allow name to be set by options
+//     const name = options.name ? options.name : 'fulcrum-provider' // ENV var instead?
+//     const Model = options.model ? options.model : path.join(__dirname, 'model.js')
+//     const routes = options.routes ? options.routes : path.join(__dirname, 'routes.js')
+//     const Controller = options.controller ? options.controller : path.join(__dirname, 'controllers.js')
   
-    return {
-        name,
-        type: 'provider',
-        version: require(path.join(__dirname, 'package.json')).version,
-        Model,
-        hosts: true,
-        disableIdParam: false,
-        routes,
-        Controller
-      }
+//     return {
+//         name,
+//         type: 'provider',
+//         version: require(path.join(__dirname, 'package.json')).version,
+//         Model,
+//         hosts: true,
+//         disableIdParam: false,
+//         routes,
+//         Controller
+//       }
+//   }
+
+
+const fulcrum = {
+    type: 'provider',
+    name: 'fulcrum',
+    Model: require('./model'),
+    version: require('./package.json').version
   }
+  
+  module.exports = fulcrum
--- model.js
+++ model.js
@@ -1,35 +1,27 @@
 require('dotenv').config()
+const { translate } = require('./utils')
 const axios = require('axios') // request is now deprecated
 
-class Model {}
+function Model () {}
 
-Model.prototype.getData = (req, callback) => {
-  const { params: { host, id }, query: { where } } = request
+Model.prototype.getData = (req, callback) => { // if this comes from express is not posible to make a http get
+  // using axios because request is now deprecated
+  const options = {
+      method:"get"
+  }
+  const url = "http://localhost:3002/api/v2/query?"
+  const token = req.params.id
 
-  // Use 'host' and 'id' to build URL to remote API
-  request(`https://${host}/resource/${id}.geojson`, (err, res, geojson) => {
-
-    if (err) return callback(err)
-    //  geojson.filtersApplied = { where: true }
-    geojson.metadata = { geometryType: 'Point' }
+  axios(`${url}token=${token}&format=geojson`, options)
+  .then(({data})=>{
+    // Set metadata used by Koop
+    const geojson = translate(data)
+    geojson.metadata = geojson.metadata || {}
+    geojson.metadata.geometryType = geojson.features[0].geometry.type || "Point"
     callback(null, geojson)
   })
-
+    // if the http request fails, return and callback with error
+  .catch(callback)
 }
-
-// Model.prototype.getData = (req, callback) => { // if this comes from express is not posible to make a http get
-//   // using axios because request is now deprecated
-//   const options = {
-//       method:"get"
-//   }
-//   axios(process.env.GEOJSON_API_URL, options)
-//   .then(({data:geojson})=>{
-//     // Set metadata used by Koop
-//     geojson.metadata = { geometryType: 'Point' }
-//     callback(null, geojson)
-//   })
-//     // if the http request fails, return and callback with error
-//   .catch(callback)
-// }
 
 module.exports = Model
